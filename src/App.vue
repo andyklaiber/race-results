de13a3664c3497615b0a1d8b75f3879ee024ba16
Andy Klaiber <andy.klaiber@gmail.com>
2022-03-23 15:00:01 -0700

    wip

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+import ResultRow from './components/ResultRow.vue';
+
+let dataUrl = '/public/data/pcrs-race-1.json'
+if(import.meta.env.DEV){
+    dataUrl = "http://localhost:3000"+dataUrl;
+}
+
+export default {
+  components: {
+    ResultRow
+  },
+  data() {
+    return {
+  	categories: {}
+    }
+  },
+  mounted: function(){
+  	let $this = this
+    fetch(dataUrl)
+        .then(response => response.json())
+        .then(data => {
+        $this.categories = data.categories
+        })
+	}
+}
+</script>
+
+<template>
+<div class="container-fluid text-center">
+    <h2>2022 Prairie City Race Series</h2>
+    <h2>Race Results</h2>
+</div>
+<div>
+    <template  v-for="(cat, key) in categories" :key="cat.id">
+    <a :href="'#'+key">{{cat.catdispname}}</a>&nbsp
+    </template>
+</div>
+<div v-for="(cat, key) in categories" :key="cat.id">
+    <h3 :id="key" >{{cat.catdispname}}</h3>
+  <table class="table table-striped table-hover">
+    <thead>
+    <tr>
+      <th v-for="(columnName, index) in cat.columns" scope="col" :key="index">{{columnName}}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(racer, idx) in cat.results" :key="idx" >
+        <ResultRow :totLaps="cat.laps" :Pos="idx" :data="racer" />
+    </tr>
+  </tbody>
+</table>
+</div>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
---
+++ src/components/ResultRow.vue
@@ -0,0 +1,31 @@
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+  props: ['data',"Pos","totLaps"],
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed:{
+      incompleteLaps(){
+          let filler = new Array(this.totLaps - this.data.laps.length);
+          return filler.fill('-') 
+      }
+  }
+}
+</script>
+
+<template>
+  <td>{{Pos + 1}}</td>
+  <td>{{data.Bib}}</td>
+  <td>{{data.Name}}</td>
+  <td>{{data.Sponsor}}</td>
+  <td v-for="(lap, idx) in data.laps" :key="idx">{{lap.timeString}}</td>
+  <template v-if="totLaps-data.laps.length > 0">
+      <td v-for="fill in incompleteLaps">{{fill}}</td>
+  </template>
+  <td>{{data.Time}}</td>
+  <td>{{data.back || "-"}}</td>
+
+</template>
